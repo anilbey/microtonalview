99e8754c69d637961f3141e37432a4691b1e3a5c
Anil Tuncel <anil.tuncel@epfl.ch>
2024-01-06 23:20:18 +0100

    add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod fps;
+use std::env;
+
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 use fps::FPSPlugin;
-
 
 fn add_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
@@ -17,11 +18,27 @@ fn add_line(mut commands: Commands) {
             ..default()
         },
         Fill::color(Color::CYAN),
-        Stroke::new(Color::Rgba { red: (1.0), green: (0.6), blue: (0.2), alpha: (0.8) }, 1.0),
+        Stroke::new(
+            Color::Rgba {
+                red: (1.0),
+                green: (0.6),
+                blue: (0.2),
+                alpha: (0.8),
+            },
+            1.0,
+        ),
     ));
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: program <features.csv> <audio.wav>");
+        return;
+    }
+    let features_path = &args[1];
+    let audio_path = &args[2];
+
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.4, 0.6, 0.6)))
         .add_systems(Startup, add_camera)
