af0381a3c6585ff1702cbd67edcfd81ac1242884
Anil Tuncel <anil.tuncel@epfl.ch>
2024-01-06 22:22:55 +0100

    rename meaningfully

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin, DiagnosticsStore},
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
     prelude::*,
 };
 
@@ -16,13 +16,11 @@ struct GreetTimer(Timer);
 struct FpsText;
 
 pub struct FPSPlugin;
-
 impl Plugin for FPSPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
             .add_plugins(FrameTimeDiagnosticsPlugin::default())
-            .add_systems(Startup, add_camera)
-            .add_systems(Startup, add_people)
+            .add_systems(Startup, add_fps_textbox)
             .add_systems(Update, update_fps);
     }
 }
@@ -31,29 +29,24 @@ fn add_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn add_people(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-
+fn add_fps_textbox(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Create an entity for displaying FPS
-    commands.spawn(TextBundle {
-        text: Text::from_section(
-            "FPS: 0",
-            TextStyle {
-                font: asset_server.load("fonts/FiraSans-Bold.ttf"), // Ensure this font is in your assets
-                font_size: 30.0,
-                color: Color::WHITE,
-            },
-        ),
-        ..Default::default()
-    })
-    .insert(FpsText);
+    commands
+        .spawn(TextBundle {
+            text: Text::from_section(
+                "FPS: 0",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"), // Ensure this font is in your assets
+                    font_size: 30.0,
+                    color: Color::WHITE,
+                },
+            ),
+            ..Default::default()
+        })
+        .insert(FpsText);
 }
 
-
-fn update_fps(
-    diagnostics: Res<DiagnosticsStore>,
-    mut query: Query<&mut Text, With<FpsText>>,
-) {
+fn update_fps(diagnostics: Res<DiagnosticsStore>, mut query: Query<&mut Text, With<FpsText>>) {
     for mut text in query.iter_mut() {
         if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
             if let Some(average) = fps.average() {
@@ -63,10 +56,10 @@ fn update_fps(
     }
 }
 
-
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.4, 0.6, 0.6)))
+        .add_systems(Startup, add_camera)
         .add_plugins((DefaultPlugins, FPSPlugin))
         .run();
 }
