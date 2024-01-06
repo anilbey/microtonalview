4caccfb4fa75ce356400089fb2fae806e03ce315
Anil Tuncel <anil.tuncel@epfl.ch>
2024-01-06 23:46:08 +0100

    read AudioAnalysisData via polars

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,18 @@ use std::env;
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 use fps::FPSPlugin;
+use polars::{
+    chunked_array::ops::ChunkAgg,
+    io::{csv::CsvReader, SerReader},
+};
+
+#[derive(Resource)]
+struct AudioAnalysisData {
+    min_frequency: f64,
+    max_frequency: f64,
+    min_loudness: f64,
+    max_loudness: f64,
+}
 
 fn add_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
@@ -39,8 +51,37 @@ fn main() {
     let features_path = &args[1];
     let audio_path = &args[2];
 
+    // Read CSV file using Polars
+    let df = CsvReader::from_path(features_path)
+        .expect("Failed to read CSV file")
+        .finish()
+        .expect("Failed to load CSV data");
+
+    let min_frequency = df
+        .column("frequency")
+        .unwrap()
+        .f64()
+        .unwrap()
+        .min()
+        .unwrap();
+    let max_frequency = df
+        .column("frequency")
+        .unwrap()
+        .f64()
+        .unwrap()
+        .max()
+        .unwrap();
+    let min_loudness = df.column("loudness").unwrap().f64().unwrap().min().unwrap();
+    let max_loudness = df.column("loudness").unwrap().f64().unwrap().max().unwrap();
+
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.4, 0.6, 0.6)))
+        .insert_resource(AudioAnalysisData {
+            min_frequency,
+            max_frequency,
+            min_loudness,
+            max_loudness,
+        })
         .add_systems(Startup, add_camera)
         .add_plugins((DefaultPlugins, FPSPlugin))
         .add_plugins(ShapePlugin)
