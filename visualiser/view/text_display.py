5e96ceec4e5a024567a728e7d6621f05f2e8fff1
Anil Tuncel <anil.tuncel@epfl.ch>
2024-08-11 17:14:04 +0200

    fix type error in text_display

--- visualiser/view/text_display.py
+++ visualiser/view/text_display.py
@@ -5,6 +5,6 @@ from view.color import Color
 
 def fps_textbox(clock: pygame.time.Clock, font_size=36, color=Color.BLACK) -> pygame.Surface:
     fps = clock.get_fps()
-    font = pygame.font.SysFont(None, font_size)
+    font = pygame.font.SysFont("freesansbold", font_size)
     fps_text = font.render(f"{fps:.2f} FPS", True, color)
     return fps_text
